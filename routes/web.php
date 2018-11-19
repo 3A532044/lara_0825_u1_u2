1a73d9acf0150e5a7408b2adc19ee9848dbc96cb
林宥丞 <a26913700@gmail.com>
2018-11-20 00:25:18 +0800

    練習 6-1：取得 Collection

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,8 @@
 
 Route::get('/', function () {
     //return view('welcome');
-    \App\Post::destroy(3, 5, 7);
+    $allPosts = \App\Post::all();
+    dd($allPosts);
+    $featuredPosts = \App\Post::where('is_feature', 1)->get();
+    dd($featuredPosts);
 });
